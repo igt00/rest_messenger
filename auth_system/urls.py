98a8b2a533d42889bee32cbe3795f835b27d7dc2
igt00 <chupilko.vika2015@gmail.com>
2021-11-06 13:32:03 +0300

    replaced imports

--- auth_system/urls.py
+++ auth_system/urls.py
@@ -1,6 +1,5 @@
-from django.urls import path
-
 from auth_system.views import LoginAPIView, LogoutAPIView, RegisterCreateAPIView
+from django.urls import path
 
 urlpatterns = [
     path('login/', LoginAPIView.as_view(), name='login'),
