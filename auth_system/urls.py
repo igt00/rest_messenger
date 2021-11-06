aa6591950c9bf503ef7b07fb01d72d8ea9a0b1f4
igt00 <chupilko.vika2015@gmail.com>
2021-11-06 12:19:16 +0300

    added I005 to ignore

--- auth_system/urls.py
+++ auth_system/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from auth_system.views import LoginAPIView, LogoutAPIView, RegisterCreateAPIView
 
-
 urlpatterns = [
     path('login/', LoginAPIView.as_view(), name='login'),
     path('logout/', LogoutAPIView.as_view(), name='logout'),
