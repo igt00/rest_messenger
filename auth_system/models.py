8e6ffe7cca392f1b23fb5af59db1976ac2070abc
igt00 <chupilko.vika2015@gmail.com>
2021-11-06 13:51:06 +0300

    added AutoHistoryMixin

--- auth_system/models.py
+++ auth_system/models.py
@@ -2,9 +2,10 @@ from django.conf import settings
 from django.db import models
 
 from auth_system.choices import GenderChoices
+from auth_system.mixins import AutoHistoryMixin
 
 
-class User2(models.Model):
+class User2(AutoHistoryMixin):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.PROTECT, verbose_name='Django User',
     )
